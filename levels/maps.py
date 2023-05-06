2b00c53fd0e927dc790fe32a994339a9125e98ba
Francisco Gabriel <francisco_abada@hotmail.com>
2023-05-06 20:24:23 -0300

    First item structure and new map layout

--- levels/maps.py
+++ levels/maps.py
@@ -1,43 +1,25 @@
 import pygame
 
-screenWidth, screenHeight = 1920, 1080
+screenWidth, screenHeight = 1920, 1080 # 1920x1080 (Full HD)
 
-layout = ['................',
-          '................',
-          '................',
-          '................',
-          'xxxxxxxxxxx.....',
-          '................',
-          '................',
-          '..............xx',
-          '................',
-          'xxxxxxxxxxxxxxxx'
-          ]
+layout = ['................................',
+          '................................',
+          '.....xxxx.......................',
+          '...............................I',
+          'xx..................xxxxxxxxxxxx',
+          '.................xxxx...........',
+          '.....xxxxxxxxxxxxx..............',
+          'x...............................',
+          'xx..............................',
+          'xxx.............................',
+          'xxxxxxxxxxxxxxxxx....xx.........',
+          '..........................xx....',
+          '................................',
+          '..............................xx',
+          '.......xx....xx....xx....xxxxxxx',
+          '................................',
+          '.P..............................',
+          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx']
 
-class Level1():
-    def __init__(self,largura, altura):
-        self.largura = largura
-        self.altura = altura
-        self.posicoesValidas = []  
-
-
-    def desenhar(self):
-        self.tela = pygame.display.set_mode((self.largura, self.altura),0)
-
-        blocoWidth = self.largura / 16
-        blocoHeight = self.altura / 12
-        for linha in enumerate(layout):
-            for coluna in range(0,16):
-                x = coluna * blocoWidth
-                y = linha[0] * blocoHeight
-                bloco = layout[linha[0]][coluna]
-                cor = (0,0,0)
-
-                if bloco == "x":
-                    bloco_rect = pygame.Rect((x,y),(blocoWidth, blocoHeight))
-                    cor = (255, 255, 0)
-                    self.posicoesValidas.append(bloco_rect)
-
-                pygame.draw.rect(self.tela, cor, ((x,y),(blocoWidth, blocoHeight)), 2)
-        
-        pygame.display.update()
+tile_Width = screenWidth / 32
+tile_Height = screenHeight / 18
