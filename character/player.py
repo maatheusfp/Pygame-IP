f780d1e5525feca918a43fee8ac12c7a329a3c15
Francisco Gabriel <francisco_abada@hotmail.com>
2023-05-07 22:07:51 -0300

    Adeed character sprites

--- character/player.py
+++ character/player.py
@@ -1,5 +1,35 @@
 import pygame 
 
+R1 = pygame.image.load("assets/characterSprites/R1.png")
+R2 = pygame.image.load("assets/characterSprites/R2.png")
+R3 = pygame.image.load("assets/characterSprites/R3.png")
+R4 = pygame.image.load("assets/characterSprites/R4.png")
+R5 = pygame.image.load("assets/characterSprites/R5.png")
+R6 = pygame.image.load("assets/characterSprites/R6.png")
+R7 = pygame.image.load("assets/characterSprites/R7.png")
+R8 = pygame.image.load("assets/characterSprites/R8.png")
+R1 = pygame.transform.scale(R1, (70, 120))
+R2 = pygame.transform.scale(R2, (70, 120))
+R3 = pygame.transform.scale(R3, (70, 120))
+R4 = pygame.transform.scale(R4, (70, 120))
+R5 = pygame.transform.scale(R5, (70, 120))
+R6 = pygame.transform.scale(R6, (70, 120))
+R7 = pygame.transform.scale(R7, (70, 120))
+R8 = pygame.transform.scale(R8, (70, 120))
+andando_direita = [R1, R2, R3, R4, R5, R6, R7, R8]
+
+andando_esquerda = [pygame.transform.flip(R1, True, False), 
+                    pygame.transform.flip(R2, True, False), 
+                    pygame.transform.flip(R3, True, False), 
+                    pygame.transform.flip(R4, True, False), 
+                    pygame.transform.flip(R5, True, False), 
+                    pygame.transform.flip(R6, True, False), 
+                    pygame.transform.flip(R7, True, False), 
+                    pygame.transform.flip(R8, True, False)]
+parado_direita = pygame.image.load("assets/characterSprites/standing.png")
+parado_direita = pygame.transform.scale(parado_direita, (60, 120))
+parado_esquerda = pygame.transform.flip(parado_direita, True, False)
+
 class Player():
     def __init__(self, x, y, width, height):
         #cria o retangulo do jogador
@@ -11,20 +41,47 @@ class Player():
         self.velocidade_Y = 4
         self.aceleracao = 2
 
+        # variáveis de controle de animação
+        self.esquerda = False
+        self.direita = False
+        self.contagem_passos = 0
+        self. direcao = 1 ######
+
         #variáveis de controle de pulo
         self.pulando = False
         self.altura_pulo = 100
         self.contador_pulo = 0
         self.podePular = True
+
+#    def draw(self, surface):
+#        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+    def draw(self, surface):
+
+#        if self.contagem_passos + 1 >= 25:
+#            self.contagem_passos = 0
+        self.contagem_passos = self.contagem_passos % 24
+
+        if self.esquerda:
+            surface.blit(andando_esquerda[self.contagem_passos//3], (self.rect.x, self.rect.y))
+            self.contagem_passos += 1
+        elif self.direita:
+            surface.blit(andando_direita[self.contagem_passos//3], (self.rect.x, self.rect.y))
+            self.contagem_passos += 1
+        else:
+            if self.direcao == 1:
+                surface.blit(parado_direita, (self.rect.x, self.rect.y))
+            elif self.direcao == -1:
+                surface.blit(parado_esquerda, (self.rect.x, self.rect.y))
+            self.contagem_passos = 0
+
     
     def moveX(self, dx):
         self.rect.x += dx * self.velocidade
     
+
     def moveY(self, dy):
         self.rect.y += dy * self.velocidade_Y
 
-    def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
 
     def check_collision(self, player_rect, obstacles, dx, dy):
         temp_rect = player_rect.copy()
@@ -41,6 +98,7 @@ class Player():
                 if dy == -1 and obstacle.bottom >= player_rect.top:
                     return True
         return False
+    
 
     def gravidade(self, obstacles):
         if not self.check_collision(self.rect, obstacles, 0, 1):
@@ -51,6 +109,7 @@ class Player():
         else:
             self.podePular = True
 
+
     def controle(self, obstacles):
 
         self.gravidade(obstacles)
@@ -59,15 +118,30 @@ class Player():
         dx, dy = 0, 0
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             dx = -1
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.esquerda = True
+            self.direita = False
+            self.direcao = -1
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]: #elif
             dx = 1
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            dy = 1 
+            self.esquerda = False
+            self.direita = True
+            self.direcao = 1
+        if not (keys[pygame.K_LEFT] or keys[pygame.K_a]) and not (keys[pygame.K_RIGHT] or keys[pygame.K_d]):
+            self.esquerda = False
+            self.direita = False
+            self.contagem_passos = 0
+        '''else:
+            self.esquerda = False
+            self.direita = False
+            self.contagem_passos = 0'''
 
         if keys[pygame.K_SPACE] and self.podePular:
-            if not self.pulando:
-                self.pulando = True
+            if not self.pulando: 
+                self.pulando = True 
                 self.contador_pulo = 0
+                self.esquerda = False
+                self.direita = False
+                self.contagem_passos = 0
                 dy = -1
 
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
@@ -80,11 +154,11 @@ class Player():
         if not self.check_collision(self.rect, obstacles, 0, dy):
             self.moveY(dy)
         
-        if self.pulando:
+        if self.pulando: 
             dy = -1
             self.contador_pulo += 1
             if self.contador_pulo > self.altura_pulo:
-                self.pulando = False
+                self.pulando = False 
 
         if not self.check_collision(self.rect, obstacles, dx, 0):
             self.moveX(dx)
@@ -93,4 +167,3 @@ class Player():
             self.moveY(dy) 
 
         return dx, dy
-
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from character.player import Player
 from levels.maps import *
 from assets.background import GameBackground
+from itens.itens import Time, Door, Key
 import sys
 
 pygame.display.set_caption("Jogo Plataforma") # Nome da janela
@@ -10,8 +11,14 @@ if __name__ == '__main__':
     pygame.init()
 
 clock = pygame.time.Clock() #frames por segundo
-obstacles = []
 screen = pygame.display.set_mode((screenWidth, screenHeight))
+game_background = GameBackground(screenWidth, screenHeight)
+
+obstacles = []
+itens = []
+keys = []
+qtdTime = []
+qtdKeys = []
 
 x = y = 0
 for row in layout:
@@ -20,13 +27,24 @@ for row in layout:
             obstacle_rect = pygame.Rect(x, y, tile_Width, tile_Height)
             obstacles.append(obstacle_rect)
         if col == 'P':
-            player_rect = pygame.Rect(x, y, tile_Width, tile_Height)
-        
+            player_rect = pygame.Rect(x, y, tile_Width, tile_Height * 2)
+        if col == 't':
+            time_rect = pygame.Rect(x,y, tile_Width, tile_Height)
+            itens.append(time_rect)
+        if col == 'k':
+            key_rect = pygame.Rect(x, y, tile_Width, tile_Height)
+            keys.append(key_rect)
+        if col == 'd':
+            door_rect = pygame.Rect(x, y, tile_Width * 2, tile_Height * 2)
+
+
         x += tile_Width
     y += tile_Height
     x = 0 
-game_background = GameBackground(screenWidth, screenHeight)
+
+door = Door(door_rect.x, door_rect.y, door_rect.width, door_rect.height)
 player = Player(player_rect.x, player_rect.y, player_rect.width, player_rect.height)
+
 
 def main():
     while True:
@@ -39,15 +57,33 @@ def main():
 
         game_background.update()
         screen.blit(game_background.screen, (0, 0))
+
+        for item in itens:
+            time_item = Time(item.x, item.y, item.width, item.height)
+            if time_item.colisao(player.rect):
+                itens.remove(item)
+                qtdTime.append(1)
+            time_item.draw(screen)
         
+        for key in keys:
+            key_item = Key(key_rect.x, key_rect.y, key_rect.width, key_rect.height)
+            if key_item.colisao(player.rect):
+                keys.remove(key)
+                qtdKeys.append(1)
+            key_item.draw(screen)
+        
+        door.draw(screen)
+        if door.colisao(player_rect):
+            pygame.quit()
+            sys.exit()
+
         for obstacle in obstacles:
             pygame.draw.rect(screen, (0, 0, 255), obstacle)
             
         player.draw(screen)
-        
-        clock.tick(100)
-        pygame.display.update()
 
+        clock.tick(60)
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
